8470b86f02421df0b26df598ccd6c83e7cd3bc71
aman-tunerlabs <aman@tunerlabs.com>
2020-04-03 17:52:30 +0530

    migrations script updated

--- migrations/20200317131858-userCreateForm.js
+++ migrations/20200317131858-userCreateForm.js
@@ -1,90 +1,80 @@
 module.exports = {
   async up(db) {
-    global.migrationMsg = "User create form"
+    global.migrationMsg = "User creation form"
+    
     let userCreateForm =
-      await db.collection('forms').findOne({ name: "userCreateForm" });
+    await db.collection('forms').findOne({ name: "userCreateForm" });
+
     if (!userCreateForm) {
 
       let allFields = [];
+      
       let inputFields = ["firstName", "lastName", "email", "phoneNumber", 
       "userName", "password","state","organisations","roles"];
+
+      let inputField = {
+        "field" : "",
+        "value" : "",
+        "visible" : true,
+        "editable" : true,
+        "label" : "",
+        "input" : "text",
+        "validation" : [{
+          "name" : "required",
+					"validator" : "required",
+					"message" : ""
+        },{
+					"name" : "pattern",
+					"validator" : "^[A-Za-z]+$/",
+					"message" : ""
+        }]
+      };
+
       await Promise.all(inputFields.map(async function (fields) {
 
-        let inputObj = {};
-       
+        let inputObj = JSON.parse(JSON.stringify(inputField));
+        let field = fields.replace( /([A-Z])/g, " $1" );
+        inputObj.label = field.charAt(0).toUpperCase() + field.slice(1);
         inputObj.field = fields;
-        inputObj.value = "";
-        inputObj.visible = true;
-        inputObj.editable = true;
-        inputObj.validation = [];
-
 
         let message = "";
         let validator = "";
-        if (fields == "password") {
 
-          inputObj.label = "Password";
-          inputObj.input = "password";
-
+        if ( fields == "password" ) {
+          inputObj.input = fields;
           validator = "^(?=.*\d).{4,8}$";
           message = "Minimum four charaters required";
- 
-         
         } else if (fields == "email") {
-          inputObj.label = "Email";
-          inputObj.input = "text";
           validator = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
           message = "Please provide a valid Email";
 
-
         } else if (fields == "phoneNumber") {
-          inputObj.label = "Phone Number";
-          inputObj.input = "text";
           validator = "(0/91)?[7-9][0-9]{9}";
           message = "Please provide a valid Phone Number";
 
-        } else if(fields =="state" || fields =="organisations" || fields =="roles" ){
+        } else if(
+          fields === "state" || 
+          fields === "organisations" ||
+          fields === "roles" 
+          ) {
 
+            inputObj.input = "select";
+            inputObj.options = [];
+            
+            if( fields === "organisations"){
+              inputObj.visible = false;
+            }
 
-          inputObj.label =fields.charAt(0).toUpperCase() + fields.slice(1);
-          inputObj.input = "select";
-          inputObj.options = [];
-          validator = "";
-          message = "";
-          if( fields =="organisations"){
-            inputObj.visible = false;
-          }
-
-        }else{
-         
-          if(fields == "firstName"){
-            inputObj.label = "First name";
-
-          }else if(fields == "lastName"){
-            inputObj.label = "Last name";
-
-          }else if(fields == "userName"){
-            inputObj.label = "User Name";
-          }
-          inputObj.input = "text";
-          validator = "([a-zA-Z]{3,30}\s*)+";
-          message = "Please provide a valid "+inputObj.label;
-
+        } else {
+          validator = inputObj.validation[1].validator;
+          message = "Please Provide Valid "+inputObj.label;
         }
 
-        inputObj.validation.push({
-          name: "required",
-          validator: "required",
-          message: inputObj.label+" required"
-        });
-
-        let patternValidation = {
-          name: "pattern",
-          validator: validator,
-          message:message
-        }
-        inputObj.validation.push(patternValidation);
+        inputObj.validation[0].message = inputObj.label+" required";
+        inputObj.validation[1].message = message;
+        inputObj.validation[1].validator = validator;
         allFields.push(inputObj);
+
       }));
 
       let createForm = {
--- migrations/20200319180540-add-platform-roles.js
+++ migrations/20200319180540-add-platform-roles.js
@@ -2,124 +2,124 @@ module.exports = {
   async up(db) {
     global.migrationMsg = "add platform roles"
 
-
-    let rolesObj = {
-      "roles": [
+    let roles = [
         {
-          "actionGroups": [
-            { "name": "Course Mentor", "id": "COURSE_MENTOR", "actions": [] }
-          ], "name": "Course Mentor", "id": "COURSE_MENTOR"
-        }
-        ,
-        {
-          "actionGroups": [
-            { "name": "Content Creation", "id": "CONTENT_CREATION", "actions": [] },
-            { "name": "Content Curation", "id": "CONTENT_CURATION", "actions": [] },
-            { "name": "Content Review", "id": "CONTENT_REVIEW", "actions": [] }
-          ],
-          "name": "Content Reviewer", "id": "CONTENT_REVIEWER"
+          "name": "Course Mentor", 
+          "id": "COURSE_MENTOR"
         },
         {
-          "actionGroups": [
-            { "name": "System Administration", "id": "SYSTEM_ADMINISTRATION", "actions": [] },
-            { "name": "Org Management", "id": "ORG_MANAGEMENT", "actions": [] }
-          ],
-          "name": "Admin", "id": "ADMIN"
+          "name": "Content Reviewer", 
+          "id": "CONTENT_REVIEWER"
         },
         {
-          "actionGroups": [
-            { "name": "Teacher Badge Issuer", "id": "TEACHER_BADGE_ISSUER", "actions": [] }
-          ],
-          "name": "Teacher Badge Issuer", "id": "TEACHER_BADGE_ISSUER"
+          "name": "Admin", 
+          "id": "ADMIN"
         },
         {
-          "actionGroups": [
-            { "name": "Org Management", "id": "ORG_MANAGEMENT", "actions": [] },
-            { "name": "Membership Management", "id": "MEMBERSHIP_MANAGEMENT", "actions": [] }
-          ],
-          "name": "Org Admin", "id": "ORG_ADMIN"
+          "name": "Teacher Badge Issuer", 
+          "id": "TEACHER_BADGE_ISSUER"
+        },
+        {
+          "name": "Org Admin", 
+          "id": "ORG_ADMIN"
         }, {
-          "actionGroups": [
-            { "name": "Book Creator", "id": "BOOK_CREATOR", "actions": [] }
-          ],
-          "name": "Book Creator", "id": "BOOK_CREATOR"
+          "name": "Book Creator", 
+          "id": "BOOK_CREATOR"
         },
         {
-          "actionGroups": [
-            { "name": "Book Reviewer", "id": "BOOK_REVIEWER", "actions": [] }
-          ],
-          "name": "Book Reviewer", "id": "BOOK_REVIEWER"
+          "name": "Book Reviewer", 
+          "id": "BOOK_REVIEWER"
         },
         {
-          "actionGroups": [
-            { "name": "Official TextBook Badge Issuer", "id": "OFFICIAL_TEXTBOOK_BADGE_ISSUER", "actions": [] }
-          ],
-          "name": "Official TextBook Badge Issuer", "id": "OFFICIAL_TEXTBOOK_BADGE_ISSUER"
+          "name": "Official TextBook Badge Issuer", 
+          "id": "OFFICIAL_TEXTBOOK_BADGE_ISSUER"
         },
         {
-          "actionGroups": [
-            {
-              "name": "Course Creator", "id": "COURSE_CREATOR", "actions": []
-            }], "name": "Course Creator", "id": "COURSE_CREATOR"
-        }, { "actionGroups": [{ "name": "Course Admin", "id": "COURSE_ADMIN", "actions": [] }], "name": "Course Admin", "id": "COURSE_ADMIN" }, { "actionGroups": [{ "name": "Membership Management", "id": "MEMBERSHIP_MANAGEMENT", "actions": [] }], "name": "Org Moderator", "id": "ORG_MODERATOR" }, { "actionGroups": [{ "name": "Public", "id": "PUBLIC", "actions": [] }], "name": "Public", "id": "PUBLIC" }, { "actionGroups": [{ "name": "Announcement Sender", "id": "ANNOUNCEMENT_SENDER", "actions": [] }], "name": "Announcement Sender", "id": "ANNOUNCEMENT_SENDER" }, { "actionGroups": [{ "name": "Program Manager", "id": "PROGRAM_MANAGER", "actions": [] }], "name": "Program Manager", "id": "PROGRAM_MANAGER" }, { "actionGroups": [{ "name": "Assessor", "id": "ASSESSOR", "actions": [] }], "name": "Assessor", "id": "ASSESSOR" }, { "actionGroups": [{ "name": "Content Creation", "id": "CONTENT_CREATION", "actions": [] }], "name": "Content Creator", "id": "CONTENT_CREATOR" }, { "actionGroups": [{ "name": "Project Manager", "id": "PROJECT_MANAGER", "actions": [] }], "name": "Project Manager", "id": "PROJECT_MANAGER" },
-        { "actionGroups": [{ "name": "Lead Assessor", "id": "LEAD_ASSESSOR", "actions": [] }], "name": "Lead Assessor", "id": "LEAD_ASSESSOR" },
-        {
-          "actionGroups": [
-            { "name": "Flag Reviewer", "id": "FLAG_REVIEWER", "actions": [] }], "name": "Flag Reviewer", "id": "FLAG_REVIEWER"
+           "name": "Course Creator", 
+           "id": "COURSE_CREATOR"
         }, {
-          "name": "Platform Admin", "id": "PLATFORM_ADMIN"
+           "name": "Course Admin", 
+           "id": "COURSE_ADMIN" 
+        }, { 
+           "name": "Org Moderator", 
+           "id": "ORG_MODERATOR" 
+        }, { 
+           "name": "Public", 
+           "id": "PUBLIC" 
+        }, { 
+          "name": "Announcement Sender", 
+          "id": "ANNOUNCEMENT_SENDER" 
+        }, {  
+          "name": "Program Manager", 
+          "id": "PROGRAM_MANAGER" 
+        }, { 
+          "name": "Assessor", 
+          "id": "ASSESSOR" 
+        }, {  
+          "name": "Content Creator", 
+          "id": "CONTENT_CREATOR" 
+        }, {  
+          "name": "Project Manager", 
+          "id": "PROJECT_MANAGER" 
+        },{ 
+          "name": "Lead Assessor", 
+          "id": "LEAD_ASSESSOR" 
+        },{
+          "name": "Flag Reviewer", 
+          "id": "FLAG_REVIEWER"
+        }, {
+          "name": "Platform Admin", 
+          "id": "PLATFORM_ADMIN"
         }
       ]
-    }
 
+    
+    let result = {
+      "createdAt" : new Date,
+      "createdBy" : "SYSTEM",
+      "updatedBy" : "SYSTEM",
+      "status" : "active",
+      "isDeleted": false,
+      "platformRole" : false,
+      "programRole" : false,
+      "customRole" : false 
+    }  
 
     let platFormRoles = [];
-    await Promise.all(rolesObj.roles.map(async function (roleInfo) {
 
+    await Promise.all(roles.map(async function (role) {
 
-      let role = {
-        "createdAt": new Date,
-        "createdBy": "SYSTEM",
-        "updatedBy": "SYSTEM",
-        "status": "active",
-        "isDeleted": false,
-      }
-      role["code"] = roleInfo.id;
-      role["title"] = roleInfo.name;
+      let platformRoleObj = {...result};
 
-      if (roleInfo.id == "LEAD_ASSESSOR" || roleInfo.id == "ASSESSOR" || roleInfo.id == "PROGRAM_MANAGER") {
+      platformRoleObj["code"] = role.id;
+      platformRoleObj["title"] = role.name;
+
+      if ( 
+        role.id == "LEAD_ASSESSOR" || 
+        role.id == "ASSESSOR" || 
+        role.id == "PROGRAM_MANAGER" 
+      ) {
+        platformRoleObj["programRole"] = true;
+        platformRoleObj["customRole"] = true;
         
-        role["platformRole"] = false;
-        role["programRole"] = true;
-        role["customRole"] = true;
-        
-      }else if (roleInfo.id == "PLATFORM_ADMIN") {
-        role["platformRole"] = false;
-        role["programRole"] = false;
-        role["customRole"] = true;
+      } else if ( role.id == "PLATFORM_ADMIN" ) {
+        platformRoleObj["customRole"] = true;
         
       } else {
-        role["platformRole"] =  true;
-        role["programRole"] = false;
-        role["customRole"] = false;
+        platformRoleObj["platformRole"] =  true;
       } 
-      platFormRoles.push(role);
+      platFormRoles.push(platformRoleObj);
 
     }));
 
-    let dbRoles = await db.collection("platformRolesExt").insertMany(platFormRoles);
+    await db.collection("platformRolesExt").insertMany(platFormRoles);
     db.collection('users').createIndex({ platformRole: 1 });
 
-    let updateUserRoles = ["OBS_REVIEWER", "OBS_DESIGNER"];
+    let updateUserRoles = [ "OBS_REVIEWER", "OBS_DESIGNER" ];
 
     await Promise.all(updateUserRoles.map(async function (userRole) {
-     let role  = {
-        "platformRole": false,
-        "programRole": false,
-        "customRole": true
-      }
 
-      let roles = db.collection("platformRolesExt").findOneAndUpdate({ code: userRole }, {
+      db.collection("platformRolesExt").findOneAndUpdate({ code: userRole }, {
         $set: {
           "platformRole": false,
           "programRole": false,
